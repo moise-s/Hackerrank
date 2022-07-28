6dbbe28c53af10386a9e92225be07ca3bc4b9caf
Moisés do Nascimento <moises.d.nasci@gmail.com>
2022-07-28 16:26:37 -0300

    init

---
+++ Python If-Else.py
@@ -0,0 +1,54 @@
+# Task
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+# Input Format
+
+# A single line containing a positive integer, .
+
+# Constraints
+
+# Output Format
+
+# Print Weird if the number is weird. Otherwise, print Not Weird.
+
+# Sample Input 0
+
+# 3
+# Sample Output 0
+
+# Weird
+# Explanation 0
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    # n = 5
+    
+    if n>=1&n<=100:
+        if n%2==1:
+            print("Weird")
+        elif n % 2 == 0 and 2 <= n <= 5:
+            print("Not Weird")
+        elif n % 2 == 0 and 6 <= n <= 20:
+            print("Weird")
+        else:
+            print("Not Weird")
